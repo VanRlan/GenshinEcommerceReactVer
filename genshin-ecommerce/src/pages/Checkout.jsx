14cabc2a9e8486d27ef9dd2728828f0977762eeb
Van Rlan <vrlan@r2hstudent.org>
2022-03-29 04:00:48 -0400

    product2

--- genshin-ecommerce/src/pages/Checkout.jsx
+++ genshin-ecommerce/src/pages/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import products from "../data/products.json";
 
 const Checkout = () => {
   return (
